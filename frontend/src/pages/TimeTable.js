6f0c178c10dcb178753e56da08cde422a8163c4d
Ki-Tak <lee08tain@gmail.com>
2024-11-10 21:11:07 +0900

    [Feat] 시간표 구현

--- frontend/src/pages/TimeTable.js
+++ frontend/src/pages/TimeTable.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import { useState, useEffect } from "react";
-import { FlexBox, WidthBlock, Wrapper } from "../styles/styled";
+import { FlexBox, GridBox, WidthBlock, Wrapper } from "../styles/styled";
 import { uploadS3 } from "../utils/upload";
 import { apiGetTimeTable, apiUploadFile } from "../apis";
 import useLogin from "../hooks/useLogin";
 import arrow from "../images/arrow-right-solid.svg";
 import add from "../images/Add_Time.svg";
+import runKirin from "../images/runningkirin.png";
 function Timetable() {
     //useLogin();
     const token = sessionStorage.getItem('token');
@@ -25,16 +26,104 @@ function Timetable() {
                             "schedule": [
                                 {
                                     "day": "월요일",   // 요일
-                                    "time": "1500 ~ 1630",   // 시작 시간
+                                    "time": "15:00 ~ 16:30",   // 시작 시간
                                     "location": "센B209" // 강의실
                                 },
                                 {
                                     "day": "수요일",
-                                    "time": "1500 ~ 1630",
+                                    "time": "15:00 ~ 16:30",
                                     "location": "센B209" // 강의실
                                 }
                             ]
                         },
+                        {
+                            "courseName": "데이터베이스",
+                            "professor_name": "김철수",
+                            "schedule": [
+                                {
+                                    "day": "화요일",
+                                    "time": "11:00 ~ 12:30",
+                                    "location": "센B210"
+                                }
+                            ]
+                        },
+                        {
+                            "courseName": "데이터베이스",
+                            "professor_name": "김철수",
+                            "schedule": [
+                                {
+                                    "day": "화요일",
+                                    "time": "12:30 ~ 15:00",
+                                    "location": "센B210"
+                                }
+                            ]
+                        },
+                        {
+                            "courseName": "데이터베이스",
+                            "professor_name": "김철수",
+                            "schedule": [
+                                {
+                                    "day": "목요일",
+                                    "time": "11:00 ~ 12:30",
+                                    "location": "센B210"
+                                }
+                            ]
+                        },
+                        {
+                            "courseName": "데이터베이스",
+                            "professor_name": "김철수",
+                            "schedule": [
+                                {
+                                    "day": "금요일",
+                                    "time": "14:00 ~ 17:00",
+                                    "location": "센B210"
+                                }
+                            ]
+                        },
+                        {
+                            "courseName": "데이터베이스",
+                            "professor_name": "김철수",
+                            "schedule": [
+                                {
+                                    "day": "금요일",
+                                    "time": "10:00 ~ 12:00",
+                                    "location": "센B210"
+                                }
+                            ]
+                        },
+                        {
+                            "courseName": "데이터베이스",
+                            "professor_name": "김철수",
+                            "schedule": [
+                                {
+                                    "day": "화요일",
+                                    "time": "17:00 ~ 19:00",
+                                    "location": "센B210"
+                                }
+                            ]
+                        },
+                        {
+                            "courseName": "데이터베이스",
+                            "professor_name": "김철수",
+                            "schedule": [
+                                {
+                                    "day": "수요일",
+                                    "time": "09:00 ~ 11:00",
+                                    "location": "센B210"
+                                }
+                            ]
+                        },
+                        {
+                            "courseName": "데이터베이스",
+                            "professor_name": "김철수",
+                            "schedule": [
+                                {
+                                    "day": "월요일",
+                                    "time": "10:00 ~ 12:00",
+                                    "location": "센B210"
+                                }
+                            ]
+                        }
                     ]
                 },
                 {
@@ -46,7 +135,7 @@ function Timetable() {
                             "schedule": [
                                 {
                                     "day": "화요일",
-                                    "time": "1100 ~ 1230",
+                                    "time": "11:00 ~ 12:30",
                                     "location": "센B210"
                                 }
                             ]
@@ -85,7 +174,35 @@ function Timetable() {
             ]
         }
     ]);
-    const handleClick = (index) => {
+    const dayMap = {
+        "월요일": 2,
+        "화요일": 3,
+        "수요일": 4,
+        "목요일": 5,
+        "금요일": 6,
+    };
+    const colorList = [
+        "rgb(255, 183, 0)",   // 머스타드
+        "rgb(86, 120, 255)",   // 아쿠아블루
+        "rgb(39, 174, 96)",  // 초록색
+        "rgb(212, 160, 255)", // 연보라
+        "rgb(230, 126, 34)", // 다크 오렌지
+        "rgb(182, 197, 255)", //밝은 파랑
+        "rgb(192, 57, 43)",  // 진한 빨강
+        "rgb(255, 138, 140)", // 밝은 빨강
+        "rgb(52, 152, 219)", // 진한 청색 
+        "rgb(170, 170, 170)"   // 밝은 회색
+    ];
+
+    // 시작 시간과 종료 시간을 기반으로 그리드 행 계산
+    const calculateGridRow = (startTime, endTime) => {
+        const [startHour, startMin] = startTime.split(":").map(Number);
+        const [endHour, endMin] = endTime.split(":").map(Number);
+        const startRow = (startHour - 9) * 2 + (startMin >= 30 ? 1 : 0); // 30분이면 한 칸 더 아래로
+        const endRow = (endHour - 9) * 2 + (endMin >= 30 ? 1 : 0);
+        return { startRow, endRow };
+    };
+    const handleListClick = (index) => {
         setSelectedIndex(index); // 클릭한 항목의 인덱스를 상태에 저장
     };
     const fetchTimetable = async () => {
@@ -148,13 +265,58 @@ function Timetable() {
                     </User>
                     <ListBlock>
                         {timetable[0].semesters.map((semester, index) => (
-                            <List key={index} className={selectedIndex === index ? 'select' : 'wait'} onClick={() => handleClick(index)} >
+                            <List key={index} className={selectedIndex === index ? 'select' : 'wait'} onClick={() => handleListClick(index)} >
                                 Let's Kirin 추천 {semester.semester}
                             </List>
                         ))}
                     </ListBlock>
                 </TimeList>
-                <TableBlock></TableBlock>
+                <TableBlock>
+                    <DayHeader style={{ gridColumn: '1 / span 1' }}><img src={runKirin} alt="KirinLogo" /></DayHeader>
+                    <DayHeader style={{ gridColumn: '2 / span 1' }}>월요일</DayHeader>
+                    <DayHeader style={{ gridColumn: '3 / span 1' }}>화요일</DayHeader>
+                    <DayHeader style={{ gridColumn: '4 / span 1' }}>수요일</DayHeader>
+                    <DayHeader style={{ gridColumn: '5 / span 1' }}>목요일</DayHeader>
+                    <DayHeader style={{ gridColumn: '6 / span 1' }}>금요일</DayHeader>
+                    {Array.from({ length: 13 }, (_, index) => {
+                        const hour = 9 + index
+                        const timeLabel = `${String(hour).padStart(2, '0')}:00`;
+                        return (
+                            <TimeSlot key={index} style={{ gridColumn: '1', gridRow: `${index * 2 + 2} / ${index * 2 + 4}` }}>{timeLabel}</TimeSlot>
+                        );
+                    })}
+                    {Array.from({ length: 13 }, (_, index) => {
+                        return Array.from({ length: 5 }, (_, i) => { // 5는 2부터 6까지의 열 수
+                            return (
+                                <TimeSlot
+                                    key={`${index}-${i}`} // 고유한 키값 생성
+                                    style={{
+                                        gridColumn: `${i + 2}`, // 2부터 6까지
+                                        gridRow: `${index * 2 + 2} / ${index * 2 + 4}`
+                                    }}
+                                ></TimeSlot>
+                            );
+                        });
+                    })}
+
+                    {timetable[0].semesters[selectedIndex].courses.map((course, index) =>
+                        course.schedule.map((schedule, sIndex) => {
+                            const [startTime, endTime] = schedule.time.split(" ~ ");
+                            const { startRow, endRow } = calculateGridRow(startTime, endTime);
+                            return (
+                                <CourseBlock
+                                    key={`${index}-${sIndex}`}
+                                    day={dayMap[schedule.day]}
+                                    startRow={startRow + 2}  // CSS grid는 1부터 시작
+                                    endRow={endRow + 2}
+                                    color={colorList[index]}
+                                >
+                                    {course.courseName} <br /> {course.professor_name} <br /> {schedule.location} {index}
+                                </CourseBlock>
+                            );
+                        })
+                    )}
+                </TableBlock>
             </TimeTableWidthBlock>
         </Wrapper>
     )
@@ -232,12 +394,6 @@ const TimeList = styled(FlexBox)`
     font-family: sans-serif;
     overflow-y: hidden;
 `
-const TableBlock = styled(FlexBox)`
-    width: 70%;
-    height: 85%;
-    border: 2px solid black;
-    position: relative;
-`
 const User = styled(FlexBox)`
     width: 100%;
     height: 6%;
@@ -278,14 +434,16 @@ const List = styled(FlexBox)`
     border-radius: 32px;
     justify-content: center;
     align-items: center;
-    font-size: 0.8rem;
+    font-size: 0.75rem;
     font-weight: 600;
     cursor: pointer;
     transition: transform 0.5s ease, background-color 0.5s ease; 
     &.select{
         color: white;
         background-color: black;
-        border: 1px solid white;
+        border: 1px solid black;
+        transform: translateY(-6px);
+        box-shadow: 4px 10px 10px rgba(0, 0, 0, 0.2);
     }
     &.wait{
         color: black;
@@ -294,6 +452,52 @@ const List = styled(FlexBox)`
     }
     &:hover{
         transform: scale(1.05);
-        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2); 
+        transform: translateY(-6px);
+        box-shadow: 4px 10px 10px rgba(0, 0, 0, 0.2); 
     }
+`
+const TableBlock = styled(GridBox)`
+    width: 70%;
+    height: 85%;
+    grid-template-columns: 0.5fr repeat(5, 1fr); /* 시간 열 + 5일 */
+    grid-template-rows: repeat(27, 1fr); /* 1시간 단위로 13행 */
+    border: 0.8px solid #BEBEBE;
+    position: relative;
+`
+const DayHeader = styled(FlexBox)`
+    align-items: center;
+    justify-content: center;
+    font-weight: 700;
+    font-size: 0.7rem;
+    background-color: #F1F1F1;
+    border-right: 0.8px solid #BEBEBE;
+    &:first-of-type{
+        border:0px;
+        border-right: 0.8px solid #BEBEBE;
+    }
+    img{
+        width: 1.8rem;
+        height: auto;
+    }
+`
+const TimeSlot = styled(FlexBox)`
+    align-items: center;
+    justify-content: center;
+    font-size: 0.6rem;
+    border: 0.8px solid #BEBEBE;
+    border-left: 0; 
+    border-bottom: 0;
+`
+const CourseBlock = styled(FlexBox)`
+    background-color: ${(props) => props.color || '#fff'};
+    grid-column: ${(props) => props.day};
+    grid-row: ${(props) => props.startRow} / ${(props) => props.endRow};
+    align-items: center;
+    justify-content: center;
+    font-weight: 500;
+    font-size: 0.6rem;
+    text-align: center;
+    overflow: hidden;
+    padding: 10px;
+    color: white;
 `
